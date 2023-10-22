fd7c9842349c8e72f83874a2cf24058e30c16709
XP <smurfs@smurfs.verymad.net>
2023-10-22 15:17:31 -0700

    Add new sims

--- src/scripts/simulation.ts
+++ src/scripts/simulation.ts
@@ -7,6 +7,8 @@ import {CustomTable} from "./tables";
 import {camel2title} from "./util/strutils";
 import {sgeNewSheetSpec} from "./sims/sge_sheet_sim_mk2";
 import {astNewSheetSpec} from "./sims/ast_sheet_sim";
+import {schNewSheetSpec} from "./sims/sch_sheet_sim";
+import {whmNewSheetSpec} from "./sims/whm_new_sheet_sim";
 
 export interface SimResult {
     mainDpsResult: number;
@@ -66,13 +68,16 @@ export function getSimSpecByStub(stub: string): SimSpec<any, any> | undefined {
 export function getDefaultSims(job: JobName, level: SupportedLevel): SimSpec<any, any>[] {
     const out: SimSpec<any, any>[] = [potRatioSimSpec];
     if (job === 'WHM' && level === 90) {
-        out.push(whmSheetSpec);
+        out.push(whmNewSheetSpec);
     }
     else if (job === 'SGE' && level === 90) {
         out.push(sgeSheetSpec);
     }
     else if (job === 'AST' && level === 90) {
         out.push(astNewSheetSpec);
+    }
+    else if (job === 'SCH' && level === 90) {
+        out.push(schNewSheetSpec);
     }
     return out;
 }
@@ -183,3 +188,5 @@ registerSim(whmSheetSpec);
 registerSim(sgeSheetSpec);
 registerSim(sgeNewSheetSpec);
 registerSim(astNewSheetSpec);
+registerSim(schNewSheetSpec);
+registerSim(whmNewSheetSpec);
