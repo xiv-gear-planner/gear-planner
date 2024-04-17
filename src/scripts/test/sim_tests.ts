cf3b310321fa110fcf8e130ece2ba3d611adbeac
xiashtra <91220277+xiashtra@users.noreply.github.com>
2024-04-17 14:09:10 -0400

    Squashed commit of the following:
    commit 856887d99ba53fdb95b445c47e388bccc981a8b8
    Author: XP <smurfs@smurfs.verymad.net>
    Date:   Wed Apr 17 09:07:36 2024 -0700
    
        Apply bristle only to damaging abilities
    
    commit 92f26608c8fe155b328a7191a8313bb8ed975523
    Author: XP <smurfs@smurfs.verymad.net>
    Date:   Wed Apr 17 09:05:44 2024 -0700
    
        One-shot damage buffs and other buff support improvements
    
    commit a939e6543d903cce22d1409de580e181bf36b906
    Author: XP <smurfs@smurfs.verymad.net>
    Date:   Tue Apr 16 22:07:19 2024 -0700
    
        More oneshot buffs

--- src/scripts/test/sim_tests.ts
+++ src/scripts/test/sim_tests.ts
@@ -2,7 +2,7 @@
 import 'global-jsdom/register'
 import {it} from "mocha";
 import {SimSettings, SimSpec} from "../simulation";
-import {Buff, FinalizedAbility, GcdAbility, OgcdAbility} from "../sims/sim_types";
+import {Ability, Buff, BuffController, FinalizedAbility, GcdAbility, OgcdAbility} from "../sims/sim_types";
 import {CharacterGearSet} from "../gear";
 import {JobMultipliers} from "../geartypes";
 import {finalizeStats} from "../xivstats";
@@ -12,15 +12,16 @@ import {
     BaseMultiCycleSim,
     CycleProcessor,
     CycleSimResult,
+    DamageResult,
     ExternalCycleSettings,
     Rotation
 } from "../sims/sim_processors";
-import {assertClose, isClose, makeFakeSet} from "./test_utils";
+import {assertClose, makeFakeSet} from "./test_utils";
 import * as assert from "assert";
 import {Divination, Litany, Mug} from "../sims/buffs";
-import {sgeNewSheetSpec} from "../sims/sge_sheet_sim_mk2";
 import {assertSimAbilityResults, setPartyBuffEnabled, UseResult} from "./sim_test_utils";
 import {Swiftcast} from "../sims/common/swiftcast";
+import {removeSelf} from "../sims/common/utils";
 
 // Example of end-to-end simulation
 // This one is testing the simulation engine itself, so it copies the full simulation code rather than
@@ -38,6 +39,15 @@ const filler: GcdAbility = {
     name: "Glare",
     potency: 310,
     attackType: "Spell",
+    gcd: 2.5,
+    cast: 1.5
+}
+
+const weaponSkill: GcdAbility = {
+    type: 'gcd',
+    name: "WepSkill",
+    potency: 310,
+    attackType: "Weaponskill",
     gcd: 2.5,
     cast: 1.5
 }
@@ -571,5 +581,262 @@ describe('Swiftcast', () => {
         assertClose(actualAbilities[3].original.lockTime, 1.48);
         assertClose(actualAbilities[3].original.castTimeFromStart, 1.38);
 
+    });
+});
+
+const potBuff: Buff = {
+    cooldown: 60,
+    duration: 10,
+    effects: {},
+    job: 'BLU',
+    name: "Pot Buff Status",
+    beforeSnapshot<X extends Ability>(controller: BuffController, ability: X): X {
+        controller.removeStatus(potBuff);
+        return {
+            ...ability,
+            potency: ability.potency + 100
+        }
+    }
+}
+
+const potBuffAbility: GcdAbility = {
+    type: 'gcd',
+    name: "Pot Buff Ability",
+    potency: null,
+    attackType: "Spell",
+    gcd: 2.5,
+    cast: 1.0,
+    activatesBuffs: [potBuff]
+}
+
+describe('Potency Buff Ability', () => {
+    it('should increase the damage once', () => {
+        const cp = new CycleProcessor({
+            allBuffs: [],
+            cycleTime: 30,
+            stats: set.computedStats,
+            totalTime: 120,
+            useAutos: false
+        });
+        cp.use(filler);
+        cp.use(potBuffAbility);
+        cp.use(filler);
+        cp.use(filler);
+        const displayRecords = cp.finalizedRecords;
+        const actualAbilities: FinalizedAbility[] = displayRecords.filter<FinalizedAbility>((record): record is FinalizedAbility => {
+            return 'ability' in record;
+        });
+        // Not swifted
+        assert.equal(actualAbilities[0].ability.name, "Glare");
+        assertClose(actualAbilities[0].directDamage, 15078, 1);
+        // Swiftcast
+        assert.equal(actualAbilities[1].ability.name, "Pot Buff Ability");
+        assertClose(actualAbilities[1].directDamage, 0, 1);
+        // Swifted
+        assert.equal(actualAbilities[2].ability.name, "Glare");
+        assertClose(actualAbilities[2].directDamage, 15078 * (310 + 100) / 310, 1);
+        // Not swifted
+        assert.equal(actualAbilities[3].ability.name, "Glare");
+        assertClose(actualAbilities[3].directDamage, 15078, 1);
+
+    });
+});
+
+function multiplyDamage(damageResult: DamageResult, multiplier: number, multiplyDirectDamage: boolean = true, multiplyDot: boolean = true) {
+    return {
+        directDamage: (damageResult.directDamage === null || !multiplyDirectDamage) ? damageResult.directDamage : {
+            expected: damageResult.directDamage.expected * multiplier
+        },
+        dot: (damageResult.dot === null || !multiplyDot) ? damageResult.dot : {
+            ...damageResult.dot,
+            damagePerTick: {
+                expected: damageResult.directDamage.expected * multiplier
+            }
+        },
+    }
+}
+
+// Demonstrates one way of doing a one-off damage increase
+const bristleBuff: Buff = {
+    // TODO
+    cooldown: 60,
+    duration: 10,
+    effects: {
+        dmgIncrease: 0.5
+    },
+    job: 'BLU',
+    name: "Bristle",
+    beforeSnapshot: removeSelf,
+    appliesTo: ability => ability.attackType === "Spell" && ability.potency !== null,
+}
+
+const bristle: GcdAbility = {
+    type: 'gcd',
+    name: "Bristle",
+    potency: null,
+    attackType: "Spell",
+    gcd: 2.5,
+    cast: 1.0,
+    activatesBuffs: [bristleBuff],
+}
+
+// Demonstrates the longer but more flexible way
+const bristleBuff2: Buff = {
+    // TODO
+    cooldown: 60,
+    duration: 10,
+    effects: {},
+    job: 'BLU',
+    name: "Bristle",
+    modifyDamage(buffController: BuffController, damageResult: DamageResult, ability: Ability): DamageResult | void {
+        if (ability.attackType === 'Spell' && ability.potency !== null) {
+            buffController.removeSelf();
+            return multiplyDamage(damageResult, 1.5, true, true);
+        }
+    }
+}
+
+const bristle2: GcdAbility = {
+    type: 'gcd',
+    name: "Bristle2",
+    potency: null,
+    attackType: "Spell",
+    gcd: 2.5,
+    cast: 1.0,
+    activatesBuffs: [bristleBuff2]
+}
+
+describe('Damage Buff Ability', () => {
+    // TODO: test a DoT
+    it('should increase the damage once', () => {
+        const cp = new CycleProcessor({
+            allBuffs: [],
+            cycleTime: 30,
+            stats: set.computedStats,
+            totalTime: 120,
+            useAutos: false
+        });
+        cp.use(filler);
+        cp.use(bristle);
+        cp.use(filler);
+        cp.use(filler);
+        const displayRecords = cp.finalizedRecords;
+        const actualAbilities: FinalizedAbility[] = displayRecords.filter<FinalizedAbility>((record): record is FinalizedAbility => {
+            return 'ability' in record;
+        });
+        // Not buffed
+        assert.equal(actualAbilities[0].ability.name, "Glare");
+        assertClose(actualAbilities[0].directDamage, 15078, 1);
+        // Buff
+        assert.equal(actualAbilities[1].ability.name, "Bristle");
+        assertClose(actualAbilities[1].directDamage, 0, 1);
+        // Buffed
+        assert.equal(actualAbilities[2].ability.name, "Glare");
+        assertClose(actualAbilities[2].directDamage, 15078 * 1.5, 1);
+        // Not buffed
+        assert.equal(actualAbilities[3].ability.name, "Glare");
+        assertClose(actualAbilities[3].directDamage, 15078, 1);
+
+    });
+    it('should increase the damage once, other style', () => {
+        const cp = new CycleProcessor({
+            allBuffs: [],
+            cycleTime: 30,
+            stats: set.computedStats,
+            totalTime: 120,
+            useAutos: false
+        });
+        cp.use(filler);
+        cp.use(bristle2);
+        cp.use(filler);
+        cp.use(filler);
+        const displayRecords = cp.finalizedRecords;
+        const actualAbilities: FinalizedAbility[] = displayRecords.filter<FinalizedAbility>((record): record is FinalizedAbility => {
+            return 'ability' in record;
+        });
+        // Not buffed
+        assert.equal(actualAbilities[0].ability.name, "Glare");
+        assertClose(actualAbilities[0].directDamage, 15078, 1);
+        // Buff
+        assert.equal(actualAbilities[1].ability.name, "Bristle2");
+        assertClose(actualAbilities[1].directDamage, 0, 1);
+        // Buffed
+        assert.equal(actualAbilities[2].ability.name, "Glare");
+        assertClose(actualAbilities[2].directDamage, 15078 * 1.5, 1);
+        // Not Buffed
+        assert.equal(actualAbilities[3].ability.name, "Glare");
+        assertClose(actualAbilities[3].directDamage, 15078, 1);
+    });
+    it('should multiply direct damage and dots by default', () => {
+        const cp = new CycleProcessor({
+            allBuffs: [],
+            cycleTime: 30,
+            stats: set.computedStats,
+            totalTime: 120,
+            useAutos: false
+        });
+        cp.use(dia);
+        cp.advanceTo(27);
+        cp.use(bristle);
+        cp.advanceTo(30);
+        cp.use(dia);
+        cp.advanceTo(60);
+        cp.use(dia);
+        // Don't cut off the final dot
+        cp.advanceTo(90);
+        const displayRecords = cp.finalizedRecords;
+        const actualAbilities: FinalizedAbility[] = displayRecords.filter<FinalizedAbility>((record): record is FinalizedAbility => {
+            return 'ability' in record;
+        });
+        // Not buffed
+        assert.equal(actualAbilities[0].ability.name, "Dia");
+        assertClose(actualAbilities[0].directDamage, 3161.5, 1);
+        assertClose(actualAbilities[0].totalDamage, 37174, 1);
+        // Buff
+        assert.equal(actualAbilities[1].ability.name, "Bristle");
+        assertClose(actualAbilities[1].directDamage, 0, 1);
+        assertClose(actualAbilities[1].totalDamage, 0, 1);
+        // Buffed
+        assert.equal(actualAbilities[2].ability.name, "Dia");
+        assertClose(actualAbilities[2].directDamage, 3161.5 * 1.5, 1);
+        assertClose(actualAbilities[2].totalDamage, 37174 * 1.5, 1);
+        // Not Buffed
+        assert.equal(actualAbilities[3].ability.name, "Dia");
+        assertClose(actualAbilities[3].directDamage, 3161.5, 1);
+        assertClose(actualAbilities[3].totalDamage, 37174, 1);
+    });
+    it('should filter abilities correctly', () => {
+        const cp = new CycleProcessor({
+            allBuffs: [],
+            cycleTime: 30,
+            stats: set.computedStats,
+            totalTime: 120,
+            useAutos: false
+        });
+        cp.use(filler);
+        cp.use(bristle);
+        // Bristle does not apply to this
+        cp.use(weaponSkill);
+        cp.use(filler);
+        cp.use(filler);
+        const displayRecords = cp.finalizedRecords;
+        const actualAbilities: FinalizedAbility[] = displayRecords.filter<FinalizedAbility>((record): record is FinalizedAbility => {
+            return 'ability' in record;
+        });
+        // Not buffed
+        assert.equal(actualAbilities[0].ability.name, "Glare");
+        assertClose(actualAbilities[0].directDamage, 15078, 1);
+        // Buff
+        assert.equal(actualAbilities[1].ability.name, "Bristle");
+        assertClose(actualAbilities[1].directDamage, 0, 1);
+        // Filtered, not buffed
+        assert.equal(actualAbilities[2].ability.name, "WepSkill");
+        assertClose(actualAbilities[2].directDamage, 15078, 1);
+        // Buffed
+        assert.equal(actualAbilities[3].ability.name, "Glare");
+        assertClose(actualAbilities[3].directDamage, 15078 * 1.5, 1);
+        // Not buffed
+        assert.equal(actualAbilities[4].ability.name, "Glare");
+        assertClose(actualAbilities[4].directDamage, 15078, 1);
     });
 });
