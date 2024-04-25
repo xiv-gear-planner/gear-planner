8ad65a5eb9079a08a5f468bada6ff6d92059417a
XP <smurfs@smurfs.verymad.net>
2024-04-25 14:31:52 -0700

    Add BiS support

--- packages/backend-resolver/src/server_builder.ts
+++ packages/backend-resolver/src/server_builder.ts
@@ -3,11 +3,27 @@ import './polyfills';
 import Fastify from "fastify";
 import {getShortLink} from "@xivgear/gearplan-frontend/external/shortlink_server";
 import {GearPlanSheet} from "@xivgear/gearplan-frontend/components";
-import {PartyBonusAmount} from "@xivgear/xivmath/geartypes";
+import {PartyBonusAmount, SheetStatsExport} from "@xivgear/xivmath/geartypes";
+import {getBisSheet} from "@xivgear/gearplan-frontend/external/static_bis";
+
+async function importExportSheet(request, rawData: string): Promise<SheetStatsExport> {
+    const exported = JSON.parse(rawData);
+    const isFullSheet = 'sets' in exported;
+    const sheet = isFullSheet ? GearPlanSheet.fromExport(exported) : GearPlanSheet.fromSetExport(exported);
+    sheet.setViewOnly();
+    const pb = request.query['partyBonus'];
+    if (pb) {
+        sheet.partyBonus = parseInt(pb) as PartyBonusAmount;
+    }
+    await sheet.loadFully();
+    return sheet.exportSheet(true, true);
+}
 
 export function buildServerInstance() {
     const fastifyInstance = Fastify({
         logger: true,
+        ignoreTrailingSlash: true,
+        ignoreDuplicateSlashes: true,
         // querystringParser: str => querystring.parse(str, '&', '=', {}),
     });
 
@@ -20,18 +36,15 @@ export function buildServerInstance() {
     })
 
     fastifyInstance.get('/fulldata/:uuid', async (request, reply) => {
-        const slData = await getShortLink(request.params['uuid']);
-        const exported = JSON.parse(slData);
-        const isFullSheet = 'sets' in exported;
-        const sheet = isFullSheet ? GearPlanSheet.fromExport(exported) : GearPlanSheet.fromSetExport(exported);
-        sheet.setViewOnly();
-        const pb = request.query['partyBonus'];
-        if (pb) {
-            sheet.partyBonus = parseInt(pb) as PartyBonusAmount;
-        }
-        await sheet.loadFully();
-        return sheet.exportSheet(true, true);
+        const rawData = await getShortLink(request.params['uuid']);
+        return importExportSheet(request, rawData);
     });
+
+    fastifyInstance.get('/fulldata/bis/:job/:expac/:sheet', async (request, reply) => {
+        const rawData = await getBisSheet(request.params['job'], request.params['expac'], request.params['sheet']);
+        return importExportSheet(request, rawData);
+    });
+
     return fastifyInstance;
 
 }
