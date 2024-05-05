ee225e0c8ef249410604e39e6fc7dd3794f8cacc
XP <smurfs@smurfs.verymad.net>
2024-05-05 09:00:47 -0700

    Add environment override

--- packages/backend-resolver/src/server.ts
+++ packages/backend-resolver/src/server.ts
@@ -1,7 +1,14 @@
 import * as process from "process";
 import {buildServerInstance} from "./server_builder";
+import {setServerOverride} from "@xivgear/gearplan-frontend/external/shortlink_server";
 
 const fastify = buildServerInstance();
+
+const backendOverride = process.env.SHORTLINK_SERVER;
+if (backendOverride) {
+    console.log(`Shortlink server override: '${backendOverride}'`);
+    setServerOverride(backendOverride);
+}
 
 fastify.listen({
     port: 30000,
