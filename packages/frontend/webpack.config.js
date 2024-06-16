fcde1c827e86743834a444e8c6466ad8d11ea76b
XP <smurfs@smurfs.verymad.net>
2024-06-15 19:22:46 -0700

    Use webpack dev server for local testing

--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -35,6 +35,13 @@ module.exports = (env, argv) => {
         ],
         resolve: {
             extensions: ['.ts', '.js'],
+        },
+        devServer: {
+            static: "./dist",
+            port: 8076,
+            client: {
+                overlay: false
+            }
         }
     }
 };
