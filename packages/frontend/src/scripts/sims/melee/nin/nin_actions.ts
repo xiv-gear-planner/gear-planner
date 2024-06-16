97a9a07314c5739dcc2b101e05e57a1db81881df
Makar <Makar8000@users.noreply.github.com>
2024-06-16 04:58:09 -0500

    add some filler logic

--- packages/frontend/src/scripts/sims/melee/nin/nin_actions.ts
+++ packages/frontend/src/scripts/sims/melee/nin/nin_actions.ts
@@ -1,6 +1,6 @@
 import {GcdAbility, OgcdAbility} from "@xivgear/core/sims/sim_types";
 import {Dokumori} from "@xivgear/core/sims/buffs";
-import {TenriJindoReady, KassatsuBuff, BunshinBuff, Higi, MeisuiBuff, KunaisBaneBuff, PhantomReady, TenChiJinReady} from './nin_buffs';
+import {TenriJindoReady, KassatsuBuff, BunshinBuff, Higi, MeisuiBuff, KunaisBaneBuff, PhantomReady, TenChiJinReady, ShadowWalker} from './nin_buffs';
 
 /**
  * Represents a Mudra Step
@@ -21,6 +21,23 @@ export type NinjutsuAbility = GcdAbility & Readonly<{
     /** Whether or not this Ninjutsu should add a Raiju stack. */
     addRaiju?: boolean,
 }>
+
+/**
+ * Represents an Ability that costs Ninki
+ */
+export type NinkiAbility = OgcdAbility & Readonly<{
+    /** The Ninki cost of this ability */
+    ninkiCost: number,
+}>
+
+/**
+ * Whether or not this ability is a Ninki spender
+ * @param action The ability to check
+ * @returns
+ */
+export function isNinkiAbility(action: NinkiAbility | OgcdAbility): action is NinkiAbility {
+    return (action as NinkiAbility).ninkiCost !== undefined;
+}
 
 /**
  * Mudras
@@ -47,8 +64,8 @@ export const SpinningEdge: GcdAbility = {
     type: 'gcd',
     name: "Spinning Edge",
     id: 2240,
-    potency: 280,
     attackType: "Weaponskill",
+    potency: 280,
     gcd: 2.5,
     cast: 0
 };
@@ -57,8 +74,8 @@ export const GustSlash: GcdAbility = {
     type: 'gcd',
     name: "Gust Slash",
     id: 2242,
-    potency: 360,
     attackType: "Weaponskill",
+    potency: 360,
     gcd: 2.5,
     cast: 0
 };
@@ -67,8 +84,8 @@ export const AeolianEdge: GcdAbility = {
     type: 'gcd',
     name: "Aeolian Edge",
     id: 2255,
-    potency: 440 + 60, // Includes Kazematoi
     attackType: "Weaponskill",
+    potency: 440 + 60, // Includes Kazematoi
     gcd: 2.5,
     cast: 0
 };
@@ -77,8 +94,8 @@ export const ArmorCrush: GcdAbility = {
     type: 'gcd',
     name: "Armor Crush",
     id: 3563,
-    potency: 460,
     attackType: "Weaponskill",
+    potency: 460,
     gcd: 2.5,
     cast: 0
 };
@@ -87,8 +104,8 @@ export const Phantom: GcdAbility = {
     type: 'gcd',
     name: "Phantom Kamaitachi",
     id: 25774,
-    potency: 600, // TODO: Mark as Pet Damage Potency
     attackType: "Weaponskill",
+    potency: 600, // TODO: Mark as Pet Damage Potency
     gcd: 2.5,
     cast: 0
 };
@@ -97,8 +114,8 @@ export const Raiju: GcdAbility = {
     type: 'gcd',
     name: "Fleeting Raiju",
     id: 25778,
-    potency: 600,
     attackType: "Weaponskill",
+    potency: 600,
     gcd: 2.5,
     cast: 0
 };
@@ -107,12 +124,12 @@ export const MudraStart: GcdAbility = {
     type: 'gcd',
     name: "Mudra Start",
     id: 2259,
+    attackType: "Ability",
     potency: null,
     cooldown: {
         time: 20,
         charges: 2
     },
-    attackType: "Ability",
     gcd: 0.5,
     fixedGcd: true,
     cast: 0
@@ -122,8 +139,8 @@ export const MudraFollowup: GcdAbility = {
     type: 'gcd',
     name: "Follow-up Mudra",
     id: 18806,
-    potency: null,
     attackType: "Ability",
+    potency: null,
     gcd: 0.5,
     fixedGcd: true,
     cast: 0
@@ -134,8 +151,8 @@ export const Fuma: NinjutsuAbility = {
     type: 'gcd',
     name: "Fuma Shuriken",
     id: 2265,
-    potency: 480,
     attackType: "Ability",
+    potency: 480,
     gcd: 1.5,
     fixedGcd: true,
     cast: 0,
@@ -146,8 +163,8 @@ export const Raiton: NinjutsuAbility = {
     type: 'gcd',
     name: "Raiton",
     id: 2267,
-    potency: 700,
     attackType: "Ability",
+    potency: 700,
     gcd: 1.5,
     fixedGcd: true,
     cast: 0,
@@ -159,11 +176,12 @@ export const Suiton: NinjutsuAbility = {
     type: 'gcd',
     name: "Suiton",
     id: 2271,
-    potency: 540,
     attackType: "Ability",
+    potency: 540,
     gcd: 1.5,
     fixedGcd: true,
     cast: 0,
+    activatesBuffs: [ShadowWalker],
     steps: [Ten, Chi, Jin]
 };
 
@@ -171,8 +189,8 @@ export const Hyosho: NinjutsuAbility = {
     type: 'gcd',
     name: "Hyosho Ranryu",
     id: 16492,
-    potency: 1300,
     attackType: "Ability",
+    potency: 1300,
     gcd: 1.5,
     fixedGcd: true,
     cast: 0,
@@ -187,11 +205,11 @@ export const KunaisBane: OgcdAbility = {
     name: "Kunai's Bane",
     // TODO: Update once available in Dawntrail
     id: 2258,
+    attackType: "Ability",
     potency: 600,
     cooldown: {
         time: 60
     },
-    attackType: "Ability",
     activatesBuffs: [KunaisBaneBuff],
 };
 
@@ -199,11 +217,11 @@ export const Kassatsu: OgcdAbility = {
     type: 'ogcd',
     name: "Kassatsu",
     id: 2264,
+    attackType: "Ability",
     potency: null,
     cooldown: {
         time: 60
     },
-    attackType: "Ability",
     activatesBuffs: [KassatsuBuff],
 };
 
@@ -211,11 +229,11 @@ export const TenChiJin: OgcdAbility = {
     type: 'ogcd',
     name: "Ten Chi Jin",
     id: 7403,
+    attackType: "Ability",
     potency: null,
     cooldown: {
         time: 120
     },
-    attackType: "Ability",
     activatesBuffs: [TenChiJinReady, TenriJindoReady],
 };
 
@@ -223,24 +241,25 @@ export const Meisui: OgcdAbility = {
     type: 'ogcd',
     name: "Meisui",
     id: 16489,
+    attackType: "Ability",
     potency: null,
     cooldown: {
         time: 120
     },
-    attackType: "Ability",
     activatesBuffs: [MeisuiBuff],
 }
 
-export const Bunshin: OgcdAbility = {
+export const Bunshin: NinkiAbility = {
     type: 'ogcd',
     name: "Bunshin",
     id: 16493,
+    attackType: "Ability",
     potency: null,
     cooldown: {
         time: 90
     },
-    attackType: "Ability",
     activatesBuffs: [BunshinBuff, PhantomReady],
+    ninkiCost: 50,
 };
 
 export const DokumoriAbility: OgcdAbility = {
@@ -248,11 +267,11 @@ export const DokumoriAbility: OgcdAbility = {
     name: "Dokumori",
     // TODO: Update once available in Dawntrail
     id: 2248,
+    attackType: "Ability",
     potency: 300,
     cooldown: {
         time: 120
     },
-    attackType: "Ability",
     activatesBuffs: [Dokumori, Higi],
 };
 
@@ -260,25 +279,27 @@ export const DreamWithin: OgcdAbility = {
     type: 'ogcd',
     name: "Dream Within a Dream",
     id: 3566,
-    potency: 150 * 3, // Multihit
     attackType: "Ability",
+    potency: 150 * 3 // Multihit
 }
 
-export const Bhavacakra: OgcdAbility = {
+export const Bhavacakra: NinkiAbility = {
     type: 'ogcd',
     name: "Bhavacakra",
     id: 7402,
-    potency: 380,
     attackType: "Ability",
+    potency: 380,
+    ninkiCost: 50
 };
 
-export const ZeshoMeppo: OgcdAbility = {
+export const ZeshoMeppo: NinkiAbility = {
     type: 'ogcd',
     name: "Zesho Meppo",
     // TODO: Update once available in Dawntrail
     id: 7402,
-    potency: 550,
     attackType: "Ability",
+    potency: 550,
+    ninkiCost: 50
 };
 
 export const TenriJindo: OgcdAbility = {
@@ -286,6 +307,6 @@ export const TenriJindo: OgcdAbility = {
     name: "Tenri Jindo",
     // TODO: Update once available in Dawntrail
     id: 7403,
-    potency: 1000,
     attackType: "Ability",
+    potency: 1000
 };
