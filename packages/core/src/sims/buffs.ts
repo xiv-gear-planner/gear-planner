ec36ba7cfe1c0dbc7c7fb1db70192130a1e83ae5
XP <smurfs@smurfs.verymad.net>
2024-06-27 16:19:28 -0700

    Dawntrail buff adjustments

--- packages/core/src/sims/buffs.ts
+++ packages/core/src/sims/buffs.ts
@@ -10,7 +10,7 @@ export const Dokumori = {
     effects: {
         dmgIncrease: 0.05,
     },
-    startTime: 3.5,
+    startTime: 3.40,
     statusId: 3849
 } as const satisfies PartyBuff;
 
@@ -18,13 +18,13 @@ export const Litany = {
     name: "Battle Litany",
     saveKey: "Battle Litany",
     job: "DRG",
-    duration: 15,
+    duration: 20,
     cooldown: 120,
     selfOnly: false,
     effects: {
         critChanceIncrease: 0.10
     },
-    startTime: 7.5,
+    startTime: 5.60,
     statusId: 786
 } as const satisfies PartyBuff;
 
@@ -48,7 +48,7 @@ export const Brotherhood = {
     name: "Brotherhood",
     saveKey: "Brotherhood",
     job: "MNK",
-    duration: 15,
+    duration: 20,
     cooldown: 120,
     selfOnly: false,
     effects: {
@@ -69,7 +69,7 @@ export const ArcaneCircle = {
     effects: {
         dmgIncrease: 0.03
     },
-    startTime: 5,
+    startTime: 4.59,
     statusId: 2599
 } as const satisfies PartyBuff;
 
@@ -92,13 +92,13 @@ export const SearingLight = {
     name: "Searing Light",
     saveKey: "Searing Light",
     job: "SMN",
-    duration: 30,
+    duration: 20,
     cooldown: 120,
     selfOnly: false,
     effects: {
         dmgIncrease: 0.03
     },
-    startTime: 1.5,
+    startTime: 4.7,
     statusId: 2703
 } as const satisfies PartyBuff;
 
@@ -112,7 +112,7 @@ export const Embolden = {
     effects: {
         dmgIncrease: 0.05
     },
-    startTime: 7,
+    startTime: 6.5,
     // This is the party member version of this, not the self version
     statusId: 1297
 } as const satisfies PartyBuff;
@@ -129,7 +129,7 @@ export const Devilment = {
         dhitChanceIncrease: 0.20,
         critChanceIncrease: 0.20
     },
-    startTime: 7,
+    startTime: 7.2,
     statusId: 1825
 } as const satisfies PartyBuff;
 
@@ -170,13 +170,13 @@ export const BattleVoice = {
     name: "Battle Voice",
     saveKey: "Battle Voice",
     job: "BRD",
-    duration: 15,
+    duration: 20,
     cooldown: 120,
     selfOnly: false,
     effects: {
         dhitChanceIncrease: 0.20,
     },
-    startTime: 6,
+    startTime: 6.4,
     statusId: 141
 } as const satisfies PartyBuff;
 
@@ -184,13 +184,13 @@ export const RadiantFinale = {
     name: "Radiant Finale",
     saveKey: "Radiant Finale",
     job: "BRD",
-    duration: 15,
+    duration: 20,
     cooldown: 120,
     selfOnly: false,
     effects: {
         dmgIncrease: 0.06
     },
-    startTime: 6.7,
+    startTime: 5.7,
     statusId: 2964
 } as const satisfies PartyBuff;
 
@@ -198,7 +198,7 @@ export const Chain = {
     name: "Chain",
     saveKey: "Chain",
     job: "SCH",
-    duration: 15,
+    duration: 20,
     cooldown: 120,
     selfOnly: false,
     effects: {
@@ -212,7 +212,7 @@ export const Divination = {
     name: "Divination",
     saveKey: "Divination",
     job: "AST",
-    duration: 15,
+    duration: 20,
     cooldown: 120,
     selfOnly: false,
     effects: {
@@ -227,7 +227,7 @@ export const AstCard = {
     saveKey: "Single Target AST Card",
     job: "AST",
     duration: 15,
-    cooldown: 30,
+    cooldown: 120,
     selfOnly: false,
     optional: true,
     effects: {
