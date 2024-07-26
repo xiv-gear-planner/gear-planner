128a74292f3bfb40a51200894d2dfa90448dc813
XP <smurfs@smurfs.verymad.net>
2024-07-25 20:38:20 -0700

    lint

--- packages/frontend/src/scripts/sims/common/potency_ratio.ts
+++ packages/frontend/src/scripts/sims/common/potency_ratio.ts
@@ -2,9 +2,8 @@ import {CharacterGearSet} from "@xivgear/core/gear";
 import {applyDhCrit, baseDamage} from "@xivgear/xivmath/xivmath";
 import {SimResult, SimSettings, SimSpec, Simulation} from "@xivgear/core/sims/sim_types";
 import {EmptyObject} from "@xivgear/core/util/types";
-import {quickElement} from "@xivgear/common-ui/components/util";
 import {NamedSection} from "../../components/section";
-import {simpleAutoResultTable, simpleMappedResultTable} from "../components/simple_tables";
+import {simpleMappedResultTable} from "../components/simple_tables";
 
 export const potRatioSimSpec: SimSpec<PotencyRatioSim, SimSettings> = {
     displayName: "Potency Ratio",
