43692afd8e0fd9d727c010d05558317cc3b234b3
XP <smurfs@smurfs.verymad.net>
2024-07-26 16:02:19 -0700

    Add doc, update type

--- packages/common-ui/src/components/delete_confirm.ts
+++ packages/common-ui/src/components/delete_confirm.ts
@@ -1,4 +1,16 @@
-export function confirmDelete(event: MouseEvent, message: string): boolean {
+/**
+ * Ask for confirmation to delete something.
+ *
+ * The confirmation can be bypassed by holding 'alt' while clicking the button.
+ *
+ * @param event The event (so that modifiers can be checked). Must have a boolean property 'altKey'.
+ * @param message The message to ask the user. Do not include the 'hold alt to bypass' notice.
+ * @returns true if the user confirms, or if alt was held to bypass confirmation. false if alt was not held and the user
+ * backed out.
+ */
+export function confirmDelete(event: {
+    altKey: boolean
+}, message: string): boolean {
     if (event.altKey) {
         return true;
     }
