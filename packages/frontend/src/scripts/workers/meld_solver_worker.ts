fd37fc72b5cb1da693e924e7af912d1d9147432a
June <kylecob1@umbc.edu>
2024-09-24 23:57:23 -0400

    Working and Fully functional!

--- packages/frontend/src/scripts/workers/meld_solver_worker.ts
+++ packages/frontend/src/scripts/workers/meld_solver_worker.ts
@@ -6,12 +6,11 @@ import {MeldSolverSettingsExport} from "../components/meld_solver_bar";
 import {MeldSolver} from "../components/meldsolver";
 import {registerDefaultSims} from "../sims/default_sims";
 
+registerDefaultSims();
 onmessage = async function (ev) {
-    console.log("starting work");
     const [sheetexp, exportedSolverSettings] = ev.data as [SheetExport, MeldSolverSettingsExport];
     const sheet = HEADLESS_SHEET_PROVIDER.fromExport(sheetexp);
     await sheet.load();
-    registerDefaultSims();
 
     const gearset = sheet.importGearSet(exportedSolverSettings.gearset);
     const sim = sheet.importSim(exportedSolverSettings.sim);
@@ -22,34 +21,5 @@ onmessage = async function (ev) {
     };
 
     const solver = new MeldSolver(sheet, solveSettings);
-    this.postMessage(await solver.solveMelds());
-    console.log("done!");
-    /*
-    let typedData = ev.data as SheetExport;
-    let sheet = new GearPlanSheet("key", typedData);
-    this.postMessage(sheet.classJobName);
-    let [solver, settings] = typedData;
-
-    if (!settings.sim) {
-        this.postMessage(null);
-        return;
-    }
-
-    let generatedSets = solver.getAllMeldCombinations(
-        solver._gearset,
-        settings.overwriteExistingMateria,
-        settings.useTargetGcd ? (settings.targetGcd ?? NORMAL_GCD) : null);
-
-    if (generatedSets.size == 0) {
-        this.postMessage(null);
-        return;
-    }
-
-    const bestSet = solver.simulateSets(
-        generatedSets,
-        settings.sim
-    );
-
-    this.postMessage(bestSet);
-    */
+    this.postMessage(sheet.exportGearSet(await solver.solveMelds()));
 }
