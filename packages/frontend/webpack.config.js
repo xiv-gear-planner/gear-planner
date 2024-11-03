75dab9c967bd77035a59f89cda626731afb065d3
XP <smurfs@smurfs.verymad.net>
2024-11-03 11:53:08 -0800

    Reduce FUOC on initial load

--- packages/frontend/webpack.config.js
+++ packages/frontend/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const path = require("path");
+const BeastiesWebpackPlugin = require("beasties-webpack-plugin");
 module.exports = (env, argv) => {
     const prod = argv.mode === 'production';
     return {
@@ -35,7 +36,14 @@ module.exports = (env, argv) => {
                 filename: "index.html",
                 inject: false
             }),
-            new NodePolyfillPlugin()
+            new NodePolyfillPlugin(),
+            new BeastiesWebpackPlugin({
+                preload: false,
+                path: './dist/',
+                publicPath: '',
+                logLevel: 'debug',
+                includeSelectors: ['body.light-mode']
+            })
         ],
         resolve: {
             extensions: ['.ts', '.js'],
