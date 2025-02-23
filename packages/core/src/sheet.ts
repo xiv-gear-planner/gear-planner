f406b346da0a2e1b2b42e1cec71fcd072420ff1a
XP <smurfs@smurfs.verymad.net>
2025-02-23 14:15:04 -0800

    Fix not being able to remove ilvl sync via 'save as'

--- packages/core/src/sheet.ts
+++ packages/core/src/sheet.ts
@@ -383,7 +383,7 @@ export class GearPlanSheet {
      * @param ilvlSync New ilvl sync. Leave unspecified or use special value 'keep' to keep existing ilvl sync.
      * @returns The saveKey of the new sheet.
      */
-    saveAs(name?: string, job?: JobName, level?: SupportedLevel, ilvlSync: number | 'keep' = 'keep'): string {
+    saveAs(name: string, job: JobName, level: SupportedLevel, ilvlSync: number | 'keep' | undefined): string {
         const exported = this.exportSheet(true);
         if (name !== undefined) {
             exported.name = name;
