5fe89e6965e90992c2aaef1cb40df25c9b9289fc
xpdota <smurfs@smurfs.verymad.net>
2025-04-18 16:26:12 -0700

    More datamanager verifications (#546)

--- packages/core/src/datamanager_new.ts
+++ packages/core/src/datamanager_new.ts
@@ -27,7 +27,12 @@ import {
 import {BaseParamToStatKey, RelevantBaseParam} from "./external/xivapitypes";
 import {getRelicStatModelFor} from "./relicstats/relicstats";
 import {requireNumber, requireString} from "./external/data_validators";
-import {DataApiClient, FoodStatBonus, GearAcquisitionSource as AcqSrc} from "@xivgear/data-api-client/dataapi";
+import {
+    DataApiClient,
+    FoodStatBonus,
+    GearAcquisitionSource as AcqSrc,
+    HttpResponse
+} from "@xivgear/data-api-client/dataapi";
 import {BaseParamMap, DataManager} from "./datamanager";
 import {IlvlSyncInfo} from "./datamanager_xivapi";
 import {applyStatCaps} from "./gear";
@@ -37,9 +42,9 @@ import {RawStatsPart} from "@xivgear/util/types";
 
 type ApiClientRawType<X extends keyof DataApiClient<never>, Y extends keyof DataApiClient<never>[X]> = DataApiClient<never>[X][Y]
 
-type ItemType = Awaited<ReturnType<ApiClientRawType<'items', 'items'>>>['data']['items'][number]
-type MateriaType = Awaited<ReturnType<ApiClientRawType<'materia', 'materia'>>>['data']['items'][number]
-type FoodType = Awaited<ReturnType<ApiClientRawType<'food', 'foodItems'>>>['data']['items'][number]
+type ApiItemData = Awaited<ReturnType<ApiClientRawType<'items', 'items'>>>['data']['items'][number]
+type ApiMateriaData = Awaited<ReturnType<ApiClientRawType<'materia', 'materia'>>>['data']['items'][number]
+type ApiFoodData = Awaited<ReturnType<ApiClientRawType<'food', 'foodItems'>>>['data']['items'][number]
 // type BaseParamType = Awaited<ReturnType<ApiClientRawType<'baseParams', 'baseParams'>>>['data']['items'][number]
 // type JobType = Awaited<ReturnType<ApiClientRawType<'jobs', 'jobs'>>>['data']['items'][number]
 // type ItemLevelType = Awaited<ReturnType<ApiClientRawType<'itemLevel', 'itemLevels'>>>['data']['items'][number]
@@ -93,6 +98,19 @@ export function setDataApi(baseUrl: string) {
     apiClient.baseUrl = baseUrl;
 }
 
+function checkResponse<X extends HttpResponse<unknown>>(response: X): X {
+    const url = response.url;
+    if (!response.ok) {
+        errorReporter(response, [url]);
+        throw new Error(`Data API error: response not ok: ${response.status}: ${response.statusText} (${url}`);
+    }
+    else if (!response.data) {
+        errorReporter(response, [url]);
+        throw new Error(`Data API error: no data: ${response.status}: ${response.statusText} (${url}`);
+    }
+    return response;
+}
+
 export class NewApiDataManager implements DataManager {
 
     private readonly _minIlvl: number;
@@ -135,7 +153,7 @@ export class NewApiDataManager implements DataManager {
             this._isyncPromise = Promise.all([baseParamPromise, this.apiClient.itemLevel.itemLevels()]).then(responses => {
                 const outMap = new Map<number, IlvlSyncInfo>();
                 const jobStats = getClassJobStats(this._classJob);
-                for (const row of responses[1].data.items) {
+                for (const row of checkResponse(responses[1]).data.items) {
                     const ilvl = row.rowId;
                     // Unroll the ItemLevel object into a direct mapping from RawStatKey => modifier
                     // BaseParam data is trickier. First, we need to convert from a list to a map, where the keys are the stat.
@@ -244,8 +262,9 @@ export class NewApiDataManager implements DataManager {
 
 
     async loadData() {
-        const baseParamPromise = this.queryBaseParams().then(data => {
-            this._baseParams = data.data.items.reduce<{
+        const baseParamPromise = this.queryBaseParams().then(response => {
+            checkResponse(response);
+            this._baseParams = response.data.items.reduce<{
                 [rawStat in RawStatKey]?: Record<OccGearSlotKey, number>
             }>((baseParams, value) => {
                 // Each individual item also gets converted
@@ -265,16 +284,17 @@ export class NewApiDataManager implements DataManager {
                 };
                 return baseParams;
             }, {});
-            return data;
+            return response;
         });
         const extraPromises: Promise<unknown>[] = [];
         console.log("Loading items");
 
 
         const itemsPromise = this.apiClient.items.items({job: this._classJob})
-            .then(async (data) => {
-                console.log(`Got ${data.data.items.length} Items`);
-                return data.data.items;
+            .then(async (response) => {
+                checkResponse(response);
+                console.log(`Got ${response.data.items.length} Items`);
+                return response.data.items;
             }).then((rawItems) => {
                 this._allItems = rawItems
                     .filter(i => {
@@ -339,9 +359,10 @@ export class NewApiDataManager implements DataManager {
         // Materia
         console.log("Loading materia");
         const materiaPromise = this.apiClient.materia.materia()
-            .then((data) => {
-                console.log(`Got ${data.data.items.length} Materia Types`);
-                this._allMateria = data.data.items
+            .then((response) => {
+                checkResponse(response);
+                console.log(`Got ${response.data.items.length} Materia Types`);
+                this._allMateria = response.data.items
                     // TODO: if a materia is discontinued but should still be available for old
                     // sets, this will not work.
                     .filter(i => i.value[MATERIA_LEVEL_MAX_NORMAL - 1])
@@ -352,9 +373,10 @@ export class NewApiDataManager implements DataManager {
             });
         console.log("Loading food");
         const foodPromise = this.apiClient.food.foodItems()
-            .then((data) => {
-                console.log(`Got ${data.data.items.length} Food Items`);
-                return data.data.items;
+            .then((response) => {
+                checkResponse(response);
+                console.log(`Got ${response.data.items.length} Food Items`);
+                return response.data.items;
             })
             .then((rawFoods) => {
                 return rawFoods.map(item => {
@@ -366,9 +388,10 @@ export class NewApiDataManager implements DataManager {
             .then((foods) => this._allFoodItems = foods);
         console.log("Loading jobs");
         const jobsPromise = this.apiClient.jobs.jobs()
-            .then(data => {
-                console.log(`Got ${data.data.items.length} Jobs`);
-                return data.data.items;
+            .then(response => {
+                checkResponse(response);
+                console.log(`Got ${response.data.items.length} Jobs`);
+                return response.data.items;
             })
             .then(rawJobs => {
                 this._jobMultipliers = new Map<JobName, JobMultipliers>();
@@ -482,7 +505,7 @@ export class DataApiGearInfo implements GearItem {
     isNqVersion: boolean;
     readonly rarity: number;
 
-    constructor(data: ItemType, forceNq: boolean = false) {
+    constructor(data: ApiItemData, forceNq: boolean = false) {
         this.isNqVersion = forceNq;
         this.id = data.rowId;
         if (forceNq) {
@@ -753,7 +776,7 @@ export class DataApiFoodInfo implements FoodItem {
     primarySubStat: RawStatKey | undefined;
     secondarySubStat: RawStatKey | undefined;
 
-    constructor(data: FoodType) {
+    constructor(data: ApiFoodData) {
         this.id = requireNumber(data.rowId);
         this.name = requireString(data.name);
         this.iconUrl = new URL(data.icon.pngIconUrl);
@@ -777,7 +800,7 @@ export class DataApiFoodInfo implements FoodItem {
     }
 }
 
-export function processRawMateriaInfo(data: MateriaType): Materia[] {
+export function processRawMateriaInfo(data: ApiMateriaData): Materia[] {
     const out: Materia[] = [];
     for (let i = MATERIA_LEVEL_MIN_RELEVANT - 1; i < MATERIA_LEVEL_MAX_NORMAL; i++) {
         const itemData = data.item[i];
