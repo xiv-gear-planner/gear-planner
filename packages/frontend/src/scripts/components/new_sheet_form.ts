e9b1af3f1d424e3459aa616c19c5d39b7336d767
XP <smurfs@smurfs.verymad.net>
2025-05-02 16:22:27 -0700

    More WIP

--- packages/frontend/src/scripts/components/new_sheet_form.ts
+++ packages/frontend/src/scripts/components/new_sheet_form.ts
@@ -14,6 +14,7 @@ import {levelSelect} from "@xivgear/common-ui/components/level_picker";
 import {BaseModal} from "@xivgear/common-ui/components/modal";
 import {SHARED_SET_NAME} from "@xivgear/core/imports/imports";
 import {recordSheetEvent} from "@xivgear/gearplan-frontend/analytics/analytics";
+import {JobIcon} from "./job_icon";
 
 export type NewSheetTempSettings = {
     ilvlSyncEnabled: boolean,
@@ -22,7 +23,8 @@ export type NewSheetTempSettings = {
 
 export class NewSheetFormFieldSet extends HTMLFieldSetElement {
     readonly nameInput: HTMLInputElement;
-    readonly jobDropdown: DataSelect<JobName>;
+    // readonly jobDropdown: DataSelect<JobName>;
+    readonly jobPicker: JobPicker;
     readonly levelDropdown: DataSelect<SupportedLevel>;
     readonly ilvlSyncCheckbox: FieldBoundCheckBox<typeof this.tempSettings>;
     readonly ilvlSyncValue: FieldBoundIntField<typeof this.tempSettings>;
@@ -45,16 +47,15 @@ export class NewSheetFormFieldSet extends HTMLFieldSetElement {
         if (defaults?.name) {
             this.nameInput.value = defaults.name;
         }
-        this.appendChild(labelFor("Sheet Name: ", this.nameInput));
-        this.appendChild(this.nameInput);
-        this.appendChild(spacer());
 
         // Job selection
-        this.jobDropdown = new DataSelect<JobName>(Object.keys(JOB_DATA) as JobName[], item => item, () => this.recheck(), defaults?.job);
-        this.jobDropdown.id = "new-sheet-job-dropdown";
-        this.jobDropdown.required = true;
-        this.appendChild(labelFor('Job: ', this.jobDropdown));
-        this.appendChild(this.jobDropdown);
+        this.jobPicker = new JobPicker(defaults.job ?? null);
+        this.appendChild(this.jobPicker);
+        this.appendChild(spacer());
+
+        // Sheet Name
+        this.appendChild(labelFor("Sheet Name: ", this.nameInput));
+        this.appendChild(this.nameInput);
         this.appendChild(spacer());
 
         // Level selection
@@ -161,7 +162,7 @@ export class NewSheetForm extends HTMLFormElement {
             return;
         }
         const nextSheetSaveStub = getNextSheetInternalName();
-        const gearPlanSheet = GRAPHICAL_SHEET_PROVIDER.fromScratch(nextSheetSaveStub, this.fieldSet.nameInput.value, this.fieldSet.jobDropdown.selectedItem, this.fieldSet.levelDropdown.selectedItem, this.fieldSet.tempSettings.ilvlSyncEnabled ? this.fieldSet.tempSettings.ilvlSync : undefined);
+        const gearPlanSheet = GRAPHICAL_SHEET_PROVIDER.fromScratch(nextSheetSaveStub, this.fieldSet.nameInput.value, this.fieldSet.jobPicker.selectedJob, this.fieldSet.levelDropdown.selectedItem, this.fieldSet.tempSettings.ilvlSyncEnabled ? this.fieldSet.tempSettings.ilvlSync : undefined);
         recordSheetEvent("newSheet", gearPlanSheet);
         this.sheetOpenCallback(gearPlanSheet).then(() => gearPlanSheet.requestSave());
     }
@@ -204,7 +205,7 @@ export class SaveAsModal extends BaseModal {
             const ilvlSyncEnabled = this.fieldSet.tempSettings.ilvlSyncEnabled;
             const ilvlSync = this.fieldSet.tempSettings.ilvlSync;
             const level: SupportedLevel = this.fieldSet.levelDropdown.selectedItem;
-            const newJob = this.fieldSet.jobDropdown.selectedItem;
+            const newJob = this.fieldSet.jobPicker.selectedJob;
             if (newJob !== undefined && newJob !== existingSheet.classJobName) {
                 const result = confirm(`You are attempting to change a sheet from ${existingSheet.classJobName} to ${newJob}. Items may need to be re-selected.`);
                 if (!result) {
@@ -231,6 +232,65 @@ function spacer() {
     return quickElement('div', ['vertical-spacer'], []);
 }
 
+class JobPicker extends HTMLElement {
+
+    private _selectedJob: JobName | null = null;
+    private _selectedSelector: HTMLButtonElement | null = null;
+
+    constructor(defaultJob: JobName | null) {
+        super();
+        const tankDiv = quickElement('div', ['job-picker-section', 'job-picker-section-tank'], []);
+        const healerDiv = quickElement('div', ['job-picker-section', 'job-picker-section-healer'], []);
+        const meleeDiv = quickElement('div', ['job-picker-section', 'job-picker-section-melee'], []);
+        const rangeDiv = quickElement('div', ['job-picker-section', 'job-picker-section-range'], []);
+        const casterDiv = quickElement('div', ['job-picker-section', 'job-picker-section-caster'], []);
+
+        const jobs = Object.keys(JOB_DATA) as JobName[];
+
+        jobs.forEach((jobName) => {
+            const job = JOB_DATA[jobName];
+            const jobSelector = quickElement('button', ['job-picker-job-icon'], [new JobIcon(jobName)]);
+            jobSelector.value = jobName;
+            jobSelector.type = 'button';
+            if (defaultJob === jobName) {
+                jobSelector.classList.add('selected');
+                this._selectedJob = jobName;
+            }
+            let parent: HTMLDivElement;
+            switch (job.role) {
+                case "Healer":
+                    parent = healerDiv;
+                    break;
+                case "Melee":
+                    parent = meleeDiv;
+                    break;
+                case "Ranged":
+                    parent = rangeDiv;
+                    break;
+                case "Caster":
+                    parent = casterDiv;
+                    break;
+                case "Tank":
+                    parent = tankDiv;
+                    break;
+            }
+            parent.appendChild(jobSelector);
+            jobSelector.addEventListener('click', (ev) => {
+                this._selectedJob = jobName;
+                this._selectedSelector?.classList.remove('selected');
+                jobSelector.classList.add('selected');
+                this._selectedSelector = jobSelector;
+            });
+        });
+        this.replaceChildren(healerDiv, tankDiv, meleeDiv, rangeDiv, casterDiv);
+    }
+
+    get selectedJob(): JobName | null {
+        return this._selectedJob;
+    }
+}
+
 customElements.define("save-as-modal", SaveAsModal);
 customElements.define("new-sheet-form-fieldset", NewSheetFormFieldSet, {extends: "fieldset"});
 customElements.define("new-sheet-form", NewSheetForm, {extends: "form"});
+customElements.define('job-picker', JobPicker);
