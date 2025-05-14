xiv-gear-planner/gear-planner

commit fac23368d257a6003a04136310467c00a289ea62

[GNB Sim] Add hypervelocity no mercy optimization for 2.50 (#544)
![image](https://github.com/user-attachments/assets/584f30f9-a6bb-4058-ade7-831962386d18)

Specifically does this for 2.50, whereas previously it'd use Brutal
Shell. Minor gain to 2.50 DPS, other speeds untouched.

packages/core/src/sims/tank/gnb/gnb_sheet_sim.ts
@@ -443,6 +443,23 @@ export class GnbSim extends BaseMultiCycleSim<GnbSimResult, GnbSettings, GnbCycl
             return Actions.BurstStrike;
         }
 
+        // If No Mercy is up after this GCD and we will only get 8 GCDs in the upcoming No Mercy, then we
+        // can use Burst Strike beforehand for a buffed Hypervelocity.
+        // We check level since there will be Burst Strike GCDs in sub 100 GNB, whereas there won't for 100 GNB.
+        if (cp.stats.level >= 100) {
+            const readyAt = cp.cdTracker.statusOf(Actions.NoMercy).readyAt;
+            // Is No Mercy coming up after this GCD?
+            if (readyAt.absolute <= cp.nextGcdTime + cp.stats.gcdPhys(2.5) - STANDARD_ANIMATION_LOCK) {
+                // Will we only get 8 GCDs in it?
+                // The 9 here counts:
+                // - The immediately following GCD (not in No Mercy)
+                // - The 8 GCDs we'll get in No Mercy
+                if (readyAt.relative + NoMercyBuff.duration <= cp.stats.gcdPhys(2.5) * 9) {
+                    return Actions.BurstStrike;
+                }
+            }
+        }
+
         if (this.willOvercapCartsNextGCD(cp)) {
             return Actions.BurstStrike;
         }

commit a2beec616538652b6790c6cddf026f5b4bf32780

[GNB] Fix logic error with gnb microclip setting, make it work in a more intuitive way (#599)
This makes the solution a lot more elegant. In essence, we will subtract
from the cooldown the difference between the ideal cooldown and the
actual cooldown. It also no longer cheats.


![image](https://github.com/user-attachments/assets/470aafd5-94cc-4cce-9054-7b67a9512a97)


Additionally this fixes a bug introduced by
https://github.com/xiv-gear-planner/gear-planner/pull/544 that caused
issues in the 1m for speeds other than 2.50

packages/core/src/sims/tank/gnb/gnb_sheet_sim.ts
@@ -347,14 +347,7 @@ export class GnbSim extends BaseMultiCycleSim<GnbSimResult, GnbSettings, GnbCycl
         if (shouldUseGCD) {
             // We are microclipping
             if (relativeCooldown > 0) {
-                // If we pretend that microclips don't exist and we have perfect
-                // alignment, we will simply use Gnashing Fang before it comes off cooldown,
-                // essentially pretending that the SkS scaling is the same. Otherwise,
-                // we should microclip so that Gnashing Fang and Double Down
-                // do not drift.
-                if (!this.settings.pretendThatMicroclipsDontExist) {
-                    cp.advanceTo(cooldown.readyAt.absolute);
-                }
+                cp.advanceTo(cooldown.readyAt.absolute);
             }
         }
         return shouldUseGCD;
@@ -454,7 +447,10 @@ export class GnbSim extends BaseMultiCycleSim<GnbSimResult, GnbSettings, GnbCycl
                 // The 9 here counts:
                 // - The immediately following GCD (not in No Mercy)
                 // - The 8 GCDs we'll get in No Mercy
-                if (readyAt.relative + NoMercyBuff.duration <= cp.stats.gcdPhys(2.5) * 9) {
+                // We check Ready To Reign to make sure it's a 2m burst. Otherwise the Burst Strike
+                // we're about to use would go in the No Mercy fully.
+                const aboutToStart2mBurst = cp.gauge.cartridges === 3 && cp.isReadyToReignBuffActive();
+                if (readyAt.relative + NoMercyBuff.duration <= cp.stats.gcdPhys(2.5) * 9 && aboutToStart2mBurst) {
                     return Actions.BurstStrike;
                 }
             }
@@ -564,6 +560,24 @@ export class GnbSim extends BaseMultiCycleSim<GnbSimResult, GnbSettings, GnbCycl
                 cp.advanceTo(cp.nextGcdTime);
             }
             gnbAbility.updateCartridges(cp.gauge);
+        }
+
+        if (this.settings.pretendThatMicroclipsDontExist) {
+            const gcd = cp.stats.gcdPhys(2.5);
+            if (gnbAbility.name === Actions.GnashingFang.name || gnbAbility.name === Actions.DoubleDown.name) {
+                // Note: this pushes us forward the ability's animation lock.
+                // For e.g. Gnashing Fang at 2.48 (29.85 CD) the cooldown without having
+                // the animation lock re-added will be 29.25.
+                const result = cp.use(ability);
+                const animationLock = gnbAbility.animationLock ?? STANDARD_ANIMATION_LOCK;
+                const cooldown = cp.cdTracker.statusOf(gnbAbility).readyAt.relative + animationLock;
+                const cooldownWithoutMicroclips = (gnbAbility.cooldown.time / cp.gcdBase) * gcd;
+                // e.g. for 2.48 this will be:
+                // 29.76 - 29.85 = -0.09
+                // which will effectively 'fix' the cooldown as if it was the ideal speed.
+                cp.cdTracker.modifyCooldown(gnbAbility, cooldownWithoutMicroclips - cooldown);
+                return result;
+            }
         }
 
         return cp.use(ability);