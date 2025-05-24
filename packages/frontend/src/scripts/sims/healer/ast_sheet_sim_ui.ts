b6790c124998ed4c6f62294ef2d182eb01732c5e
xpdota <smurfs@smurfs.verymad.net>
2025-05-24 13:40:31 -0700

    Gauge refactors (#648)
    Closes #251 
    
    This refactors nearly all of the existing gauges to use CSS instead of
    hardcoded styling.

--- packages/frontend/src/scripts/sims/healer/ast_sheet_sim_ui.ts
+++ packages/frontend/src/scripts/sims/healer/ast_sheet_sim_ui.ts
@@ -1,87 +1,69 @@
 import {DisplayRecordFinalized, isFinalizedAbilityUse} from "@xivgear/core/sims/cycle_sim";
-import {PreDmgUsedAbility} from "@xivgear/core/sims/sim_types";
 import {col, CustomColumnSpec} from "@xivgear/common-ui/table/tables";
 import {BaseMultiCycleSimGui} from "../multicyclesim_ui";
 import {AstExtraData, AstSettings, AstSimResult} from "@xivgear/core/sims/healer/ast_sheet_sim";
 import {StyleSwitcher, WritableCssProp} from "@xivgear/common-ui/util/types";
+import {extraDataDiscreteGaugeRenderer} from "../common/sim_ui_utils";
 
 export class AstSheetSimGui extends BaseMultiCycleSimGui<AstSimResult, AstSettings> {
 
 
     protected extraAbilityUsedColumns(result: AstSimResult): CustomColumnSpec<DisplayRecordFinalized, unknown, unknown>[] {
+        const cardStyles: StyleSwitcher = {
+            Balance: {
+                clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
+                background: '#ff6f00',
+            },
+            Arrow: {
+                clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
+                background: '#69ebff',
+            },
+            Spire: {
+                clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
+                background: '#fff700',
+            },
+            Lord: {
+                clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
+                background: '#bd040a',
+            },
+            Spear: {
+                clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
+                background: '#0059ff',
+            },
+            Bole: {
+                clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
+                background: '#00bd23',
+            },
+            Ewer: {
+                clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
+                background: '#00ddff',
+            },
+            Lady: {
+                clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
+                background: '#ffe4d6',
+            },
+        } as const;
         return [col({
             shortName: 'cards',
             displayName: 'Cards',
             getter: used => isFinalizedAbilityUse(used) ? used.original : null,
-            renderer: (usedAbility?: PreDmgUsedAbility) => {
-                if (usedAbility?.extraData !== undefined) {
-                    const cards = (usedAbility.extraData as AstExtraData).gauge.cards;
+            renderer: extraDataDiscreteGaugeRenderer<AstExtraData>((_, extra) => {
 
-                    const div = document.createElement('div');
-                    div.style.height = '100%';
-                    div.style.display = 'flex';
-                    div.style.alignItems = 'center';
-                    div.style.justifyContent = 'center';
-                    div.style.gap = '4px';
-                    div.style.padding = '2px 0 2px 0';
-                    div.style.boxSizing = 'border-box';
+                const children = [];
 
-                    const cardStyles: StyleSwitcher = {
-                        Balance: {
-                            clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
-                            background: '#ff6f00',
-                        },
-                        Arrow: {
-                            clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
-                            background: '#69ebff',
-                        },
-                        Spire: {
-                            clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
-                            background: '#fff700',
-                        },
-                        Lord: {
-                            clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
-                            background: '#bd040a',
-                        },
-                        Spear: {
-                            clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
-                            background: '#0059ff',
-                        },
-                        Bole: {
-                            clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
-                            background: '#00bd23',
-                        },
-                        Ewer: {
-                            clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
-                            background: '#00ddff',
-                        },
-                        Lady: {
-                            clipPath: `polygon(15% 0%, 85% 0%, 85% 100%, 15% 100%)`,
-                            background: '#ffe4d6',
-                        },
-                    } as const;
-
-                    for (const key in cardStyles) {
-                        const stack = document.createElement('span');
-                        for (const [k, v] of Object.entries(cardStyles[key as keyof typeof cardStyles])) {
-                            stack.style[k as WritableCssProp] = v;
-                        }
-                        stack.style.height = '100%';
-                        stack.style.width = '16px';
-                        stack.style.display = 'inline-block';
-                        stack.style.overflow = 'hidden';
-                        if (!cards.has(key)) {
-                            stack.style.background = '#00000033';
-                        }
-                        div.appendChild(stack);
+                const cards = extra.gauge.cards;
+                for (const key in cardStyles) {
+                    const stack = document.createElement('span');
+                    for (const [k, v] of Object.entries(cardStyles[key as keyof typeof cardStyles])) {
+                        stack.style[k as WritableCssProp] = v;
                     }
-                    ;
-
-                    return div;
+                    if (!cards.has(key)) {
+                        stack.style.background = '#00000033';
+                    }
+                    children.push(stack);
                 }
-                return document.createTextNode("");
-            },
-        }),
-        ];
+                return children;
+            }),
+        })];
     }
 }
