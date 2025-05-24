7aadf87328b64835343a994e89862c53dd07ec7c
XP <smurfs@smurfs.verymad.net>
2025-05-23 20:03:57 -0700

    Lint

--- packages/frontend/src/scripts/sims/tank/war_sheet_sim_ui.ts
+++ packages/frontend/src/scripts/sims/tank/war_sheet_sim_ui.ts
@@ -50,48 +50,47 @@ export class WarSimGui extends BaseMultiCycleSimGui<WarSimResult, WarSettings> {
                 }
                 return document.createTextNode("");
             },
-        },
-            {
-                shortName: 'surgingTempest',
-                displayName: 'Surging Tempest',
-                getter: used => isFinalizedAbilityUse(used) ? used.original : null,
-                renderer: (usedAbility?: PreDmgUsedAbility) => {
-                    if (usedAbility?.extraData !== undefined) {
-                        const surgingTempestDuration = (usedAbility.extraData as WarExtraData).surgingTempest;
-                        const div = document.createElement('div');
-                        div.style.height = '100%';
-                        div.style.display = 'flex';
-                        div.style.alignItems = 'center';
-                        div.style.gap = '6px';
-                        div.style.padding = '2px 0 2px 0';
-                        div.style.boxSizing = 'border-box';
+        }, {
+            shortName: 'surgingTempest',
+            displayName: 'Surging Tempest',
+            getter: used => isFinalizedAbilityUse(used) ? used.original : null,
+            renderer: (usedAbility?: PreDmgUsedAbility) => {
+                if (usedAbility?.extraData !== undefined) {
+                    const surgingTempestDuration = (usedAbility.extraData as WarExtraData).surgingTempest;
+                    const div = document.createElement('div');
+                    div.style.height = '100%';
+                    div.style.display = 'flex';
+                    div.style.alignItems = 'center';
+                    div.style.gap = '6px';
+                    div.style.padding = '2px 0 2px 0';
+                    div.style.boxSizing = 'border-box';
 
-                        const span = document.createElement('span');
-                        span.textContent = `${surgingTempestDuration}s`;
+                    const span = document.createElement('span');
+                    span.textContent = `${surgingTempestDuration}s`;
 
-                        const barOuter = document.createElement('div');
-                        barOuter.style.borderRadius = '20px';
-                        barOuter.style.background = '#00000033';
-                        barOuter.style.width = '120px';
-                        barOuter.style.height = 'calc(100% - 3px)';
-                        barOuter.style.display = 'inline-block';
-                        barOuter.style.overflow = 'hidden';
-                        barOuter.style.border = '1px solid black';
+                    const barOuter = document.createElement('div');
+                    barOuter.style.borderRadius = '20px';
+                    barOuter.style.background = '#00000033';
+                    barOuter.style.width = '120px';
+                    barOuter.style.height = 'calc(100% - 3px)';
+                    barOuter.style.display = 'inline-block';
+                    barOuter.style.overflow = 'hidden';
+                    barOuter.style.border = '1px solid black';
 
-                        const barInner = document.createElement('div');
-                        barInner.style.backgroundColor = '#ee9199';
-                        barInner.style.height = '100%';
-                        barInner.style.width = `${Math.round((surgingTempestDuration / 60) * 100)}%`;
-                        barOuter.appendChild(barInner);
+                    const barInner = document.createElement('div');
+                    barInner.style.backgroundColor = '#ee9199';
+                    barInner.style.height = '100%';
+                    barInner.style.width = `${Math.round((surgingTempestDuration / 60) * 100)}%`;
+                    barOuter.appendChild(barInner);
 
-                        div.appendChild(barOuter);
-                        div.appendChild(span);
+                    div.appendChild(barOuter);
+                    div.appendChild(span);
 
-                        return div;
-                    }
-                    return document.createTextNode("");
-                },
+                    return div;
+                }
+                return document.createTextNode("");
             },
+        },
         ];
     }
 
