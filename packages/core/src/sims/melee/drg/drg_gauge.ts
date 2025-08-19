3524b417758f7a0f6a8858e65e9fa1ab917c9298
Clo Yun-Hee <6630833+clo-yunhee@users.noreply.github.com>
2025-08-19 03:46:11 +0200

    Almost working DRG sim (#718)
    Works for all levels, Life Surge logic might not be correct at level 70.
    Needs to be fixed more properly because level modifier for cooldown does
    not work properly for Life Surge and I'm not sure why.

---
+++ packages/core/src/sims/melee/drg/drg_gauge.ts
@@ -0,0 +1,44 @@
+import {GaugeManager} from "@xivgear/core/sims/cycle_sim";
+import {DrgGaugeState} from "./drg_types";
+
+export class DrgGaugeManager implements GaugeManager<DrgGaugeState> {
+
+    private _level: number;
+    private _firstmindsFocus: number = 0;
+
+    constructor(level: number) {
+        this._level = level;
+    }
+
+    get level(): number {
+        return this._level;
+    }
+
+    get firstmindsFocus(): number {
+        return this._firstmindsFocus;
+    }
+
+    set firstmindsFocus(newFirstmindsFocus: number) {
+        // This gauge only exists at level 90 and above.
+        if (this.level < 90) return;
+
+        if (newFirstmindsFocus > 2) {
+            console.warn(`[DRG Sim] Overcapped Firstminds' Focus.`);
+        }
+        if (newFirstmindsFocus < 0) {
+            console.warn(`[DRG Sim] Used Firstminds' Focus with no charge.`);
+        }
+        this._firstmindsFocus = Math.max(Math.min(newFirstmindsFocus, 2), 0);
+    }
+
+    gainFirstmindsFocus() {
+        this.firstmindsFocus += 1;
+    }
+
+    gaugeSnapshot(): DrgGaugeState {
+        return {
+            level: this.level,
+            firstmindsFocus: this.firstmindsFocus,
+        };
+    }
+}
