4ddfaefea14743617bf28626b5566c1a47d91b6d
Violet Stardust <violet.stardust.xiv@gmail.com>
2025-08-18 22:40:35 -0400

    fix bug

--- packages/frontend/src/scripts/components/meld_solver_modal.ts
+++ packages/frontend/src/scripts/components/meld_solver_modal.ts
@@ -128,7 +128,7 @@ export class MeldSolverDialog extends BaseModal {
 
     async solveResultReceived(set: CharacterGearSet, dps: number) {
         const oldDps = (await this.settingsDiv.simSettings.sim.simulate(this.settingsDiv.gearsetGenSettings.gearset)).mainDpsResult;
-        const confirm = new MeldSolverConfirmationDialog(this._sheet, this.settingsDiv.gearsetGenSettings.gearset, set, [oldDps, dps], this.close);
+        const confirm = new MeldSolverConfirmationDialog(this._sheet, this.settingsDiv.gearsetGenSettings.gearset, set, [oldDps, dps], () => this.close());
         confirm.attachAndShowExclusively();
     }
 
